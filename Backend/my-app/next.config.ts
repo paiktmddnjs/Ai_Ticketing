cdebf0eeada96ebee3b4635b8955aff4e2fba34c
paiktmddnjs <paiktmddnjs@naver.com>
2026-04-30 02:14:21 +0900

    fix : cros 정책 설정

--- Backend/my-app/next.config.ts
+++ Backend/my-app/next.config.ts
@@ -1,16 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: false,
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:5173" },
+          // 💡 여기를 실제 Cloudflare 주소로 수정합니다!
+          { key: "Access-Control-Allow-Origin", value: "https://ai-ticketing.pages.dev/" }, 
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ]
